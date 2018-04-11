c064fdd3b4e214196e675052d02ef0ec1b90fbbd
Jay R Bolton <jayrbolton@gmail.com>
2018-04-10 17:17:35 -0700

    Clean up fetch_assembly module

--- lib/FastANI/fetch_assembly.py
+++ lib/FastANI/fetch_assembly.py
@@ -4,23 +4,21 @@ from AssemblyUtil.AssemblyUtilClient import AssemblyUtil
 # Fetch genome assemblies in parallel
 
 
-def fetch_assembly(callback_url, ref):
-    '''
-    Fetch a single assembly file with a workspace reference
-    '''
-    assembly_util = AssemblyUtil(callback_url)
-    return assembly_util.get_assembly_as_fasta({'ref': ref})['path']
-
-
 def fetch_multiple(callback_url, refs):
     '''
     Fetch multiple assembly files in parallel and return them in an array
     :param callback_url:
     :param refs: array of workspace references to assemblies
-    :returns: array of file path
+    :returns: array of file paths of fetched assemblies
     '''
+    assembly_util = AssemblyUtil(callback_url)
     pool = multiprocessing.Pool(processes=len(refs))
     jobs = []
     for ref in refs:
-        jobs.append(pool.apply_async(fetch_assembly, (callback_url, ref)))
+        jobs.append(pool.apply_async(fetch_assembly, (assembly_util, ref)))
     return [j.get() for j in jobs]
+
+
+def fetch_assembly(assembly_util, ref):
+    # Fetch a single assembly file with a workspace reference
+    return assembly_util.get_assembly_as_fasta({'ref': ref})['path']
